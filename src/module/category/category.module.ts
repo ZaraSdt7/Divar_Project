0398520465da5372a8d61e529b81fa2495db3c85
zahra <zahra.st7373@gmail.com>
2024-02-25 20:16:22 +0330

    update modules❗

--- src/module/category/category.module.ts
+++ src/module/category/category.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
+import { AdModule } from '../ad/ad.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
 
 @Module({
+  imports: [AdModule, TypeOrmModule.forFeature([Category])],
   controllers: [CategoryController],
   providers: [CategoryService],
 })
