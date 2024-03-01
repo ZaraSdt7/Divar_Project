a49ada9933ec1bc1e0dac80754c9cf4c9b6cfeb8
zahra <zahra.st7373@gmail.com>
2024-03-01 17:16:39 +0330

    ....

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -1,42 +1,16 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/register-dto';
-import { UpdateAuthDto } from './dto/login-dto';
+import { LoginDto } from './dto/login-dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
+@ApiTags('auth-login')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  @Post('login')
+  @HttpCode(HttpStatus.CREATED)
+  async login(@Body() createAuthDto: LoginDto) {
+    return await this.authService.login(createAuthDto);
   }
 }
