d7586c318b08f26485e32dc9e5ce39310f74791a
zahra <zahra.st7373@gmail.com>
2024-04-07 18:51:10 +0330

    start project..

--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -25,38 +25,8 @@ export class AuthService {
     });
   }
 
-  async register(register: RegisterDto): Promise<ResponseFormat<any>> {}
+  async register(register: RegisterDto): Promise<ResponseFormat<any>> {
+    
+  }
   async login(logindto: LoginDto): Promise<ResponseFormat<any>> {}
 }
-//   const existingAccount = await this.accountrepository.findOne({
-//     where: { mobile: logindto.mobile },
-//   });
-//   const otpcode = await this.smsservice.sendOtpcode(
-//     existingAccount.mobile,
-//     existingAccount.code,
-//   );
-//   if (existingAccount) {
-//     // check otpcode
-//     if (existingAccount.code !== otpcode) {
-//       throw new UnauthorizedException(
-//         ResponseMessages.CODE_SENT_IS_NOT_CORRECT,
-//       );
-//     }
-//     await this.accountrepository.save(existingAccount);
-//     return {
-//       statusCode: HttpStatus.CREATED,
-//       message: 'login successful',
-//     };
-//   } else {
-//     return {
-//       statusCode: HttpStatus.NOT_FOUND,
-//       message: 'please create account',
-//     };
-//   }
-// }
-// private generateRandomNumber(): string {
-//   const minm = 100000,
-//     maxm = 999999;
-//   const code = Math.floor(Math.random() * (maxm - minm + 1)) + minm;
-//   return String(code);
-// }
--- src/module/auth/dto/register-dto.ts
+++ src/module/auth/dto/register-dto.ts
@@ -30,4 +30,3 @@ export class RegisterDto {
   @IsNotEmpty()
   code: string;
 }
-
